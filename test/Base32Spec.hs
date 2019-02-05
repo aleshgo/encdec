e0d64437dad4e9edc1f1bd0e0b4466e202759063
Oleksii Zhuravlov <aleshgo@gmail.com>
2019-02-05 16:53:35 +0300

    Add base32 decode tests

--- test/Base32Spec.hs
+++ test/Base32Spec.hs
@@ -14,3 +14,7 @@ spec = do
       Decoder.decode (Encoded "" :: Encoded 'Base32) `shouldBe` (Ok "" :: Result ByteString)
       Decoder.decode (Encoded "MY======" :: Encoded 'Base32) `shouldBe` (Ok "f" :: Result ByteString)
       Decoder.decode (Encoded "MZXQ====" :: Encoded 'Base32) `shouldBe` (Ok "fo" :: Result ByteString)
+      Decoder.decode (Encoded "MZXW6===" :: Encoded 'Base32) `shouldBe` (Ok "foo" :: Result ByteString)
+      Decoder.decode (Encoded "MZXW6YQ=" :: Encoded 'Base32) `shouldBe` (Ok "foob" :: Result ByteString)
+      Decoder.decode (Encoded "MZXW6YTB" :: Encoded 'Base32) `shouldBe` (Ok "fooba" :: Result ByteString)
+      Decoder.decode (Encoded "MZXW6YTBOI======" :: Encoded 'Base32) `shouldBe` (Ok "foobar" :: Result ByteString)
