9b99f37dfe4d75f5f096095f3f187d6e03fc9be1
Oleksii Zhuravlov <aleshgo@gmail.com>
2019-02-07 18:47:37 +0300

    Add Result helpers

--- src/Encdec/Types.hs
+++ src/Encdec/Types.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module Encdec.Types where
 
@@ -19,7 +20,28 @@ newtype Encoded (a :: Encoding) =
 data Result a
   = Err ByteString
   | Ok a
-  deriving (Eq, Show)
+  deriving (Eq, Show, Functor)
+
+instance Applicative Result where
+  pure = Ok
+  Ok f <*> a = fmap f a
+  Err e <*> _ = Err e
+
+instance Monad Result where
+  Ok a >>= f = f a
+  Err e >>= _ = Err e
+
+result :: (ByteString -> b) -> (a -> b) -> Result a -> b
+result f _ (Err e) = f e
+result _ g (Ok a) = g a
+
+isErr :: Result a -> Bool
+isErr (Err _) = True
+isErr (Ok _) = False
+
+isOk :: Result a -> Bool
+isOk (Err _) = False
+isOk (Ok _) = True
 
 leftToErr :: ByteString -> a -> Result b
 leftToErr = const . Err
